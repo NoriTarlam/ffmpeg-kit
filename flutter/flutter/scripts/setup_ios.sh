aaba7d2728c30f5af1292e9750c436a0ee887733
NoriTarlam <68306954+NoriTarlam@users.noreply.github.com>
2025-04-02 17:08:27 +0900

    Update setup_ios.sh

--- flutter/flutter/scripts/setup_ios.sh
+++ flutter/flutter/scripts/setup_ios.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IOS_URL="https://www.wity.jp/ffmpegkit/v6.0lts/ffmpeg-kit-audio-6.0.LTS-ios-framework.zip"
+IOS_URL="https://www.wity.jp/ffmpegkit/v6.0lts/ffmpeg-kit-full-gpl-6.0.LTS-ios-framework.zip"
 mkdir -p Frameworks
 curl -L $IOS_URL -o frameworks.zip
 unzip -o frameworks.zip -d Frameworks

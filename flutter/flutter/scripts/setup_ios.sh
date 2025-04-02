8a509944819f2cdf67e68e7cb2b5fa48d8c3790b
NoriTarlam <68306954+NoriTarlam@users.noreply.github.com>
2025-04-02 16:59:02 +0900

    Create setup_ios.sh

---
+++ flutter/flutter/scripts/setup_ios.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+IOS_URL="https://www.wity.jp/ffmpegkit/v6.0lts/ffmpeg-kit-full-gpl-6.0.LTS-ios-framework.zip"
+mkdir -p Frameworks
+curl -L $IOS_URL -o frameworks.zip
+unzip -o frameworks.zip -d Frameworks
+rm frameworks.zip
